6b11678b17bc7c83b531a09262d745fc53c27743
Sean <seon2you@hotmail.com>
2017-01-10 17:24:33 -0800

    added entities

---
+++ src/PhotoGalleryNgCoreApi/Entities/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoGalleryNgCoreApi.Entities
+{
+    public class Album : IEntityBase
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual ICollection<Photo> Photos { get; set; }
+    }
+}
