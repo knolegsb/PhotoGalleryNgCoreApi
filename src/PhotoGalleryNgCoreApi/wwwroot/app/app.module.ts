dde8ba50b8e049b6457d4874a4e05f61d5932775
Sean <seon2you@hotmail.com>
2017-10-03 01:04:40 -0700

    app files added

--- src/PhotoGalleryNgCoreApi/wwwroot/app/app.module.ts
+++ src/PhotoGalleryNgCoreApi/wwwroot/app/app.module.ts
@@ -5,15 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { Location, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { Headers, RequestOptions, BaseRequestOptions } from '@angular/http';
 
-import { AccountModule } from './components/account/account.module';
+//import { AccountModule } from './components/account/account.module';
 import { AppComponent } from './app.component';
-import { AlbumPho}
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule
-    ]
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
