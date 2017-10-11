0c034ab1ca3f8eb280de79095c0aa3d2fc07bece
Sean <seon2you@hotmail.com>
2017-10-10 21:35:56 -0700

    added services account components

--- src/PhotoGalleryNgCoreApi/wwwroot/app/app.module.ts
+++ src/PhotoGalleryNgCoreApi/wwwroot/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Location, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { Headers, RequestOptions, BaseRequestOptions } from '@angular/http';
 
-//import { AccountModule } from './components/account/account.module';
+import { AccountModule } from './components/account/account.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
 import { AlbumPhotosComponent } from './components/album-photos.component';
@@ -14,14 +14,35 @@ import { PhotosComponent } from './components/photos.component';
 
 import { routing } from './routes';
 
+import { DataService } from './core/services/data.service';
+import { MembershipService } from './core/services/membership.service';
+import { UtilityService } from './core/services/utility.service';
+import { NotificationService } from './core/services/notification.service';
+
+class AppBaseRequestOptioins extends BaseRequestOptions {
+    headers: Headers = new Headers();
+
+    constructor() {
+        super();
+        this.headers.append('Content-Type', 'application/json');
+        this.body = '';
+    }
+}
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        routing
+        routing,
+        AccountModule
     ],
     declarations: [AppComponent, HomeComponent, AlbumPhotosComponent, PhotosComponent, AlbumsComponent],
+    providers: [
+        DataService, MembershipService, UtilityService, NotificationService,
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        { provide: RequestOptions, useClass: AppBaseRequestOptioins }
+    ],
     bootstrap: [AppComponent]
 })
 
--- src/PhotoGalleryNgCoreApi/wwwroot/app/components/account/account.module.ts
+++ src/PhotoGalleryNgCoreApi/wwwroot/app/components/account/account.module.ts
@@ -7,6 +7,9 @@ import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
 
 import { accountRouting } from './routes';
+import { DataService } from '../../core/services/data.service';
+import { MembershipService } from '../../core/services/membership.service';
+import { NotificationService } from '../../core/services/notification.service';
 
 @NgModule({
     imports: [
@@ -20,7 +23,9 @@ import { accountRouting } from './routes';
         RegisterComponent
     ],
     providers: [
-
+        DataService,
+        MembershipService,
+        NotificationService
     ]
 })
 export class AccountModule { }
